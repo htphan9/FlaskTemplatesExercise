3e77b5545c05e955ace12e7d00d0783ffb4ce2ff
Henry Phan <htphan9@asu.edu>
2026-02-08 12:20:25 -0700

    Added content to home and contact page, added ASU themed CSS

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const currentLocation = window.location.pathname;
-//   const navLinks = document.querySelectorAll('.nav-link');
-//   navLinks.forEach(link => {
-//     if (link.getAttribute('href') === currentLocation) {
-//       link.classList.add('active');
-//     }
-//   });
-// });
